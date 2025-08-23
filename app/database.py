6fc4c9dfc30b34ba2eee1ed283ad45e15c0fa16f
crescendonow <manowwon@gmail.com>
2025-08-23 09:42:05 +0700

    create variable database connection in railway.

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:jbWYRQvbrjPcpCXAzkOWSvJaHdCmHzWv@postgres.railway.internal:5432/railway")
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
